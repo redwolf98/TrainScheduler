5adba13c57ca5735d8542982552900bed2dc6600
Anthony Habash <redwolf98@gmail.com>
2017-10-14 11:18:02 -0400

    finalize

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -9,107 +9,128 @@
   };
   firebase.initializeApp(config);
 
-var DEBUG = true;
+var DEBUG = false;
+var trains = [];
 
 function log(text){
     if(DEBUG) console.log(text);
 }
 
-  var database = firebase.database();
+var database = firebase.database();
   
- function loadData(){
+function grabData(){
+    
+    trains = [];
+
     database.ref().on("child_added", function(snapshot){
         //Add items to the train table
+        
         var sv = snapshot.val();
-        var trainName = $("<td>").html(sv.trainName);
-        var destination = $("<td>").html(sv.destination);
-        var frequency = $("<td>").html(sv.frequency);
+        var train = {trainName:sv.trainName,
+            destination:sv.destination,
+            frequency:sv.frequency,
+            firstTrainTime:sv.firstTrainTime};
         
-        /**
-         * find difference in minutes between first arrival time and current time (timedifference)
-         * get modular of timeDifference to frequency plus 5 (for In Station) if between 
-         */
-        
-        var minutesFromInitialDepart;
-        var minutesSinceLastDeparture;
-        var nextArrival = $("<td>")
-        var minutesAway = $("<td>");
-
-        if(moment(sv.firstTrainTime, "HH:mm").isBefore(moment())){
-            log(moment(sv.firstTrainTime, "HH:mm").format("HH:mm A") + " is Before " + moment().format("HH:mm A"));
-            minutesFromInitialDepart = moment().diff(moment(sv.firstTrainTime, "HH:mm"), "minutes");
-
-            log("minutes FROm INitial Depart: " + minutesFromInitialDepart);
-            log(minutesFromInitialDepart + " % " + (parseInt(sv.frequency)));
-    
-            minutesSinceLastDeparture =Math.abs(minutesFromInitialDepart % (parseInt(sv.frequency)));
-            
-            log("last arrival: " + (minutesSinceLastDeparture))            
-            log("nextArrival:" + (parseInt(sv.frequency) - minutesSinceLastDeparture));
-    
-    
-            nextArrival.html(moment().add({minutes:(parseInt(sv.frequency) - minutesSinceLastDeparture)}).format("hh:mm A"));
-            
-            
-            
-                minutesAway = $("<td>").addClass("minutesAway").html((parseInt(sv.frequency) - minutesSinceLastDeparture));
-            
-            
-
-
-        }else{
-            log(moment(sv.firstTrainTime, "HH:mm").format("HH:mm A") + " is After " + moment().format("hh:mm A"));
-            minutesAway.html(Math.abs(moment(sv.firstTrainTime, "HH:mm").diff(moment(), "minutes")));
-
-            nextArrival.html("Service Begins at " + moment(sv.firstTrainTime,"HH:mm").format("hh:mm A"));
-
-        }
-
-        
-        var record = $("<tr>")
-        
-        record.append(trainName);
-        record.append(destination);
-        record.append(frequency);
-        record.append(nextArrival);
-        record.append(minutesAway)
-        
-        $("#trainRecords").append(record);
+        trains.push(train);
 
          // Handle the errors
         }, function(errorObject) {
             console.log("Errors handled: " + errorObject.code);
         });
- }
+}
 
-  $(document).ready(loadData());
+$(document).ready(function(){
+                                grabData();
+                                loadTrains();
+                            });
 
-    $(document).ready(function(){
-        $("#inputButton").on("click", function(event){
-            event.preventDefault();
-            var trainName = $("#input-TrainName").val().trim();
-            var destination = $("#input-Destination").val().trim();
-            var frequency = $("#input-Frequency").val().trim();
-            var firstTrainTime = $("#input-FirstTrainTime").val().trim();
+$(document).ready(function(){
+    $("#inputButton").on("click", function(event){
+        event.preventDefault();
+        var trainName = $("#input-TrainName").val().trim();
+        var destination = $("#input-Destination").val().trim();
+        var frequency = $("#input-Frequency").val().trim();
+        var firstTrainTime = $("#input-FirstTrainTime").val().trim();
 
-            
-            database.ref().push({
-                trainName:trainName,
-                destination:destination,
-                frequency:frequency,
-                firstTrainTime:firstTrainTime
-            });
+        var train = {trainName:trainName,
+            destination:destination,
+            frequency:frequency,
+            firstTrainTime:firstTrainTime};
 
-            $("#trainRecords").empty();
-            
-        });
+        database.ref().push(train);
+        $("#form-addTrain")[0].reset();
+
+
+
     });
+});
+
+function loadSingleTrain(train){
+    var trainName = $("<td>").html(train.trainName);
+    var destination = $("<td>").html(train.destination);
+    var frequency = $("<td>").html(train.frequency);
+    
+    /**
+     * find difference in minutes between first arrival time and current time (timedifference)
+     * get modular of timeDifference to frequency plus 5 (for In Station) if between 
+     */
+    
+    var minutesFromInitialDepart;
+    var minutesSinceLastDeparture;
+    var nextArrival = $("<td>")
+    var minutesAway = $("<td>");
+
+    if(moment(train.firstTrainTime, "HH:mm").isBefore(moment())){
+        log(moment(train.firstTrainTime, "HH:mm").format("HH:mm A") + " is Before " + moment().format("HH:mm A"));
+        minutesFromInitialDepart = moment().diff(moment(train.firstTrainTime, "HH:mm"), "minutes");
+
+        log("minutes FROm INitial Depart: " + minutesFromInitialDepart);
+        log(minutesFromInitialDepart + " % " + (parseInt(train.frequency)));
+
+        minutesSinceLastDeparture =Math.abs(minutesFromInitialDepart % (parseInt(train.frequency)));
+        
+        log("last arrival: " + (minutesSinceLastDeparture))            
+        log("nextArrival:" + (parseInt(train.frequency) - minutesSinceLastDeparture));
 
 
+        nextArrival.html(moment().add({minutes:(parseInt(train.frequency) - minutesSinceLastDeparture)}).format("hh:mm A"));
+        
+        
+        
+            minutesAway = $("<td>").addClass("minutesAway").html((parseInt(train.frequency) - minutesSinceLastDeparture));
+        
+        
 
-    setInterval(function(){
-                            $("#trainRecords").empty();
-                            loadData();
-                          }
-                            ,1000
-    );
+
+    }else{
+        log(moment(train.firstTrainTime, "HH:mm").format("HH:mm A") + " is After " + moment().format("hh:mm A"));
+        minutesAway.html(Math.abs(moment(train.firstTrainTime, "HH:mm").diff(moment(), "minutes")));
+
+        nextArrival.html("Service Begins at " + moment(train.firstTrainTime,"HH:mm").format("hh:mm A"));
+
+    }
+
+    
+    var record = $("<tr>")
+    
+    record.append(trainName);
+    record.append(destination);
+    record.append(frequency);
+    record.append(nextArrival);
+    record.append(minutesAway)
+    
+    $("#trainRecords").append(record);
+}
+
+function loadTrains(){
+    $("#trainRecords").empty();
+    for(i=0;i<trains.length;i++){
+        loadSingleTrain(trains[i]);
+    }
+}
+
+setInterval(function(){
+                        loadTrains();
+                        }
+                        ,1000
+);
